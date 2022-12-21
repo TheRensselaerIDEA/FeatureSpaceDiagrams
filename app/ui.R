3b2cae0ef4e17cc9668ea75bd23c32fbb1b6787d
Win Cowger, PhD <wincowger@gmail.com>
2022-12-21 10:57:35 -0500

    update plotting.

--- app/ui.R
+++ app/ui.R
@@ -3,6 +3,6 @@ fluidPage(
   fileInput(inputId = "file", label = "CSV Data File", accept = ".csv"),
   selectInput("cor_method", "Correlation Method:",
               choices = c("pearson", "spearman", "kendall")),
-  sliderInput(inputId = "line_threshold", label = "Distance Threshold", value = 0.5,  min = 0, max = 1, step = 0.1),
+  sliderInput(inputId = "line_threshold", label = "Distance Threshold", value = 5.5,  min = 0, max = 10, step = 0.1),
   plotOutput("feature_plot")
 )
